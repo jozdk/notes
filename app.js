d152108f3ca455643c745e1223872e10a50c28d4
jozdk <djannikhaezo@web.de>
2022-04-11 00:01:45 +0200

    Set up express app

---
+++ app.js
@@ -0,0 +1,42 @@
+import { default as express } from "express";
+import { engine } from "express-handlebars";
+import * as path from "path";
+import { default as logger } from "morgan";
+import { default as cookieParser } from "cookie-parser";
+import * as http from "http";
+import { approotdir } from "./approotdir";
+const __dirname = approotdir;
+import {
+    normalizePort, onError, onListening, handle404, basicErrorHandler
+} from "./appsupport"
+
+import { router as indexRouter } from "./routes/index";
+
+export const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Router functions
+app.use("/", indexRouter);
+
+// Error handlers
+app.use(handle404);
+app.use(basicErrorHandler);
+
+export const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+export const server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
