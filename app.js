43b09713a27dd86abbfe3d496eb7cca94cb96d74
jozdk <djannikhaezo@web.de>
2022-04-16 00:11:57 +0200

    Set up Bootstrap

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets/vendor/bootstrap", express.static(path.join(__dirname, "node_modules", "bootstrap", "dist")));
 
 // Router functions
 app.use("/", indexRouter);
