e776c019f1e80381b4248ba77b4990e88b13406d
jozdk <djannikhaezo@web.de>
2022-04-22 14:41:44 +0200

    Create Sequelize Model; add SequelizeNotesStore

---
+++ models/notes-sequelize.js
@@ -0,0 +1,110 @@
+import { Note, AbstractNotesStore } from "./Notes.js";
+import { Sequelize, Model, DataTypes } from "sequelize";
+import {
+    connectDB as connectSeqlz,
+    close as closeSeqlz
+} from "./sequelize,js";
+import util from "util";
+import { default as DBG } from "debug";
+
+const debug = DBG("notes:notes-sequelize");
+
+let sequelize;
+
+export class SQNote extends Sequelize.Model { }
+
+async function connectDB() {
+    if (sequelize) {
+        return;
+    }
+
+    try {
+        sequelize = await connectSeqlz();
+        SQNote.init({
+            notekey: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                unique: true
+            },
+            title: {
+                type: DataTypes.STRING,
+            },
+            body: {
+                type: DataTypes.TEXT
+            }
+        }, {
+            sequelize,
+            modelName: "SQNote"
+        });
+
+        await SQNote.sync();
+        debug(`The model SQNote was defined and its table was created`);
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+export default class SequelizeNotesStore extends AbstractNotesStore {
+    async close() {
+        closeSeqlz();
+        sequelize = undefined;
+    }
+
+    async create(key, title, body) {
+        await connectDB();
+        const sqnote = await SQNote.create({
+            notekey: key,
+            title: title,
+            body: body
+        });
+        return new Note(sqnote.notekey, sqnote.title, sqnote.body);
+    }
+
+    async read(key) {
+        await connectDB();
+        const note = await SQNote.findOne({ where: { notekey: key } });
+        debug(note);
+        if (!note) {
+            throw new Error(`No note found for ${key}`);
+        } else {
+            return new Note(note.notekey, note.title, note.body);
+        }
+    }
+
+    async update(key, title, body) {
+        await connectDB();
+        const note = await SQNote.findOne({ where: { notekey: key } });
+        if (!note) {
+            throw new Error(`No note found for ${key}`);
+        } else {
+            await SQNote.update({
+                title: title,
+                body: body
+            }, {
+                where: { notekey: key }
+            });
+            return this.read(key);
+        }
+    }
+
+    async destroy(key) {
+        await connectDB();
+        await SQNote.destroy({ where: { notekey: key } });
+        debug(`DESTROY ${key}`);
+    }
+
+    async keylist() {
+        await connectDB();
+        const notes = await SQNote.findAll({ attributes: ["notekey"] });
+        debug(`keylist: ${util.inspect(notes)}`);
+        const notekeys = notes.map((note) => note.notekey);
+        return notekeys;
+    }
+
+    async count() {
+        await connectDB();
+        const count = await SQNote.count();
+        return count;
+    }
+}
