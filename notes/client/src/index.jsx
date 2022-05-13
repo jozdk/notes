9c888d596700f8ade1c55dea28f1c087a7f72a94
jozdk <djannikhaezo@web.de>
2022-05-13 16:51:35 +0200

    Add React Router; mount index route

--- notes/client/src/index.jsx
+++ notes/client/src/index.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 
-const title = "Hello React with Webpack and Babel!";
-
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<App title={title} />)
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
+)
