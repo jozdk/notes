184209154d54b60c1afccf7915b230bba5b15005
jozdk <djannikhaezo@web.de>
2022-05-27 16:44:55 +0200

    Restyle NoteView (buttons at bottom of screen)

--- notes/client/src/components/NoteView.jsx
+++ notes/client/src/components/NoteView.jsx
@@ -42,23 +42,38 @@ export const NoteView = () => {
     }, [notekey]);
 
     return (
-        <div className="grow basis-4/5">
+        <div className="basis-4/5 flex h-[calc(100vh_-_72px)] max-h-screen">
 
             {note ? (
-                <div className="p-5">
-                    <h3 className="text-2xl font-semibold pb-1 mb-3 border-b border-grey-100">
-                        {note.title}
-                        {/* <i class="bi bi-pencil-fill text-sm text-slate-500"></i>
-                        <i class="bi bi-trash3-fill text-sm text-slate-500"></i> */}
-                    </h3>
-                    <p className="mb-10">{note.body}</p>
-                    {/* <p>Key: {notekey}</p> */}
+                <div className="p-5 mx-auto w-full 2xl:w-3/4">
+
+                    <div className="min-h-[calc(100%_-_57px)]">
+                        <div className="pb-1 mb-3 border-b border-grey-100 flex justify-between">
+                            <h3 className="text-2xl font-semibold ">{note.title}</h3>
+                            {user && notekey && (
+                                <div className="w-20 flex justify-around items-center">
+                                    <Link to={`/notes/edit/${notekey}`}>
+                                        <i className="bi bi-pencil-fill text-slate-500 cursor-pointer"></i>
+                                    </Link>
+                                    <Link to={`/notes/destroy/${notekey}`}>
+                                        <i className="bi bi-trash3-fill text-slate-500 cursor pointer"></i>
+                                    </Link>
+                                </div>
+                            )}
+                        </div>
+
+
+                        <p className="mb-10">{note.body}</p>
+                        {/* <p>Key: {notekey}</p> */}
+                    </div>
+
+
 
                     {user && notekey && (
 
                         <div className="inline-block border-t border-grey-100 py-2 pr-3">
-                            <Link className="inline-block py-2 px-4 bg-main text-black shadow-md rounded-md hover:outline hover:outline-dark mr-2" to={`/notes/destroy/${notekey}`}><i class="bi bi-trash3-fill mr-1"></i> Delete</Link>
-                            <Link className="inline-block py-2 px-4 bg-main text-black shadow-md rounded-md hover:outline hover:outline-dark" to={`/notes/edit/${notekey}`}><i class="bi bi-pencil-fill mr-1"></i> Edit</Link>
+                            <Link className="inline-block py-2 px-4 bg-main text-black shadow-md rounded-md hover:outline hover:outline-dark mr-2" to={`/notes/destroy/${notekey}`}><i className="bi bi-trash3-fill mr-1"></i> Delete</Link>
+                            <Link className="inline-block py-2 px-4 bg-main text-black shadow-md rounded-md hover:outline hover:outline-dark" to={`/notes/edit/${notekey}`}><i className="bi bi-pencil-fill mr-1"></i> Edit</Link>
                         </div>
 
 
