8db089823817279157bc0712afa20bd12ae9e43d
jozdk <djannikhaezo@web.de>
2022-06-25 01:21:08 +0200

    Create NoteNotFound component

---
+++ notes/client/src/components/NoteNotFound.jsx
@@ -0,0 +1,12 @@
+export const NoteNotFound = () => {
+    return (
+        <div style={{width: "315px"}} className="m-auto">
+            <div>
+                <img src="/assets/svg/no_note.svg" alt="Note not found" />
+            </div>
+            <div className="mt-5 text-center">
+                Note not found.
+            </div>
+        </div>
+    )
+}
