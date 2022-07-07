d1260bf91e7a4591c07a3a2e35c6a207a11aec91
jozdk <djannikhaezo@web.de>
2022-07-07 19:06:02 +0200

    Create users model for sqlite3

--- notes/server/models/notes-sqlite3.js
+++ notes/server/models/notes-sqlite3.js
@@ -1,6 +1,8 @@
 import util from "util";
 import { Note, AbstractNotesStore } from "./Notes.js";
 import { default as sqlite3 } from "sqlite3";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 import { default as DBG } from "debug";
 
 const debug = DBG("notes:notes-sqlite3");
@@ -21,15 +23,53 @@ async function connectDB() {
             (err) => {
                 if (err) return reject(err);
                 debug(`Opened SQLite3 database ${dbfile} db=${util.inspect(db)}`);
-                console.log("db: ", db);
                 resolve(db);
             }
         );
     });
+
+    await new Promise((resolve, reject) => {
+        db.run(
+            `CREATE TABLE IF NOT EXISTS notes (
+                notekey TEXT PRIMARY KEY,
+                title TEXT,
+                body TEXT,
+                createdAt TEXT,
+                updatedAt TEXT
+            )`,
+            (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                debug("Created table notes (if it not already existed)");
+                resolve();
+            }
+        );
+    });
+
+    await new Promise((resolve, reject) => {
+        db.run(
+            `CREATE TABLE IF NOT EXISTS users (
+                id TEXT PRIMARY KEY,
+                username TEXT UNIQUE,
+                password TEXT,
+                createdAt TEXT,
+                updatedAt TEXT
+            )`,
+            (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                debug("Created table users (if it not already existed)");
+                resolve();
+            }
+        );
+    })
+
     return db;
 }
 
-export default class SQLite3NotesStore extends AbstractNotesStore {
+class SQLite3NotesStore extends AbstractNotesStore {
     async close() {
         const _db = db;
         db = undefined;
@@ -76,7 +116,7 @@ export default class SQLite3NotesStore extends AbstractNotesStore {
                         return reject(new Error(`No note found for ${key}`));
                     } else {
                         const note = new Note(row.notekey, row.title, row.body, row.createdAt, row.updatedAt);
-                        debug(`READ ${util.inspect(note)}`);
+                        // debug(`READ ${util.inspect(note)}`);
                         resolve(note);
                     }
                 }
@@ -123,7 +163,6 @@ export default class SQLite3NotesStore extends AbstractNotesStore {
 
     async keylist() {
         const db = await connectDB();
-        debug(`keylist db=${util.inspect(db)}`);
         const keys = await new Promise((resolve, reject) => {
             db.all("SELECT notekey FROM notes",
                 (err, rows) => {
@@ -153,4 +192,187 @@ export default class SQLite3NotesStore extends AbstractNotesStore {
         });
         return count;
     }
+}
+
+class SQLite3UsersStore {
+    async close() {
+        const _db = db;
+        db = undefined;
+        return _db ?
+            new Promise((resolve, reject) => {
+                _db.close((err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    else resolve();
+                });
+            }) : undefined;
+    }
+
+    async create(username, password) {
+        await connectDB();
+        const id = uuidv4();
+        const date = new Date().toISOString();
+        const user = await new Promise((resolve, reject) => {
+            db.run(
+                `INSERT INTO users (id, username, password, createdAt, updatedAt)
+                    VALUES (?, ?, ?, ?, ?)`,
+                [id, username, password, date, date],
+                function (err) {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve({
+                        id: id,
+                        username,
+                        createdAt: date,
+                        updatedAt: date
+                    });
+                }
+            )
+        });
+        return user;
+    }
+
+    async read(username) {
+        await connectDB();
+        const user = await new Promise((resolve, reject) => {
+            db.get(
+                "SELECT * FROM users WHERE username = ?",
+                [username],
+                (err, row) => {
+                    if (err) {
+                        return reject(err);
+                    } else if (!row) {
+                        resolve(row);
+                    } else {
+                        resolve({
+                            id: row.id,
+                            username: row.username,
+                            createdAt: row.createdAt,
+                            updatedAt: row.updatedAt
+                        });
+                    }
+
+
+                }
+            )
+        });
+        return user;
+    }
+
+    async update(username, password) {
+        await connectDB();
+        const date = new Date().toISOString();
+        await new Promise((resolve, reject) => {
+            db.run(
+                `UPDATE users
+                    SET password = ?, updatedAt = ?
+                    WHERE username = ?`,
+                [password, date, username],
+                function (err) {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve();
+                }
+            )
+        });
+        return await this.read(username);
+    }
+
+    async destroy(username) {
+        await connectDB();
+        return await new Promise((resolve, reject) => {
+            db.run(
+                "DELETE FROM users WHERE username = ?",
+                [username],
+                (err) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    debug("DELETED ", username);
+                    resolve();
+                }
+            )
+        });
+    }
+
+    async list() {
+        await connectDB();
+        const usersList = await new Promise((resolve, reject) => {
+            db.all(
+                "SELECT * FROM users",
+                (err, rows) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(rows.map((row) => {
+                        return {
+                            id: row.id,
+                            username: row.username,
+                            createdAt: row.createdAt,
+                            updatedAt: row.updatedAt
+                        };
+                    }));
+                }
+            );
+        });
+        return usersList;
+    }
+
+    async checkPassword(username, password) {
+        await connectDB();
+        const user = await new Promise((resolve, reject) => {
+            db.get(
+                "SELECT * FROM users WHERE username = ?",
+                [username],
+                (err, row) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(row);
+                }
+            );
+        });
+
+        let checked;
+        if (!user) {
+            checked = {
+                check: false,
+                username,
+                message: "User not found"
+            };
+        } else {
+            let pwcheck = false;
+
+            if (user.username === username) {
+                pwcheck = await bcrypt.compare(password, user.password);
+            }
+
+            if (pwcheck) {
+                checked = {
+                    check: true,
+                    username: user.username
+                };
+            } else {
+                checked = {
+                    check: false,
+                    username,
+                    message: "Incorrect username or password"
+                };
+            }
+        }
+        return checked;
+    }
+}
+
+export {
+    SQLite3NotesStore as NotesStoreClass,
+    SQLite3UsersStore as UsersStoreClass
 }
--- notes/server/models/schema-sqlite3.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS notes (
-    notekey VARCHAR(255),
-    title VARCHAR(255),
-    body TEXT,
-    createdAt VARCHAR(255),
-    updatedAt VARCHAR(255)
-);
