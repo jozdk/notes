facaac3adb2939a94ceb924a1bb8c9810c66416b
jozdk <djannikhaezo@web.de>
2022-08-08 21:12:26 +0200

    Set secure attribute for cookies in production

--- server/app.js
+++ server/app.js
@@ -52,7 +52,8 @@ const sessionMiddleware = session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 30,
         httpOnly: true,
-        sameSite: true
+        sameSite: true,
+        secure: process.env.NODE_ENV === "production" ? true : false
     }
 })
 
